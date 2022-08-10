9a15d4312591940f7eb2c66b0278f58ede9436c9
edyama <ey.yamagata@gmail.com>
2022-08-10 09:20:04 -0300

    First controller and Razor pages tests

--- SalesWebMvc/Controllers/HomeController.cs
+++ SalesWebMvc/Controllers/HomeController.cs
@@ -17,7 +17,9 @@ namespace SalesWebMvc.Controllers
 
         public IActionResult About()
         {
-            ViewData["Message"] = "Your application description page.";
+            ViewData["Message"] = "Sales Web MVC App C# Course";
+            ViewData["Author"] = "Eduardo Yamagata";
+            ViewData["Email"] = "ey.yamagata@gmail.com";
 
             return View();
         }
